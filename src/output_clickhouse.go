1af502c9a37e8282235ccde0e98f01418b43559b
Rudolf Venter <mr.rudolf.venter@gmail.com>
2021-02-09 19:13:11 +0200

    debug

--- src/output_clickhouse.go
+++ src/output_clickhouse.go
@@ -161,12 +161,12 @@ func clickhouseSendData(connect clickhouse.Clickhouse, batch []DNSResult, server
 						}
 					}
 					neweTLDPlusOne, err := publicsuffix.EffectiveTLDPlusOne(string(dnsQuery.Name))
-					//if err == nil && eTLDPlusOne != "" {
-					//	eTLDPlusOne = strings.TrimRight(eTLDPlusOne, ".")
-					//} else if strings.Count(dnsQuery.Name, ".") == 1 {
+					if err == nil && eTLDPlusOne != "" {
+						neweTLDPlusOne = strings.TrimRight(eTLDPlusOne, ".")
+					} else if strings.Count(dnsQuery.Name, ".") == 1 {
 						// Handle publicsuffix.EffectiveTLDPlusOne eTLD+1 error with 1 dot in the domain.
-					//	eTLDPlusOne = dnsQuery.Name
-					//}
+						neweTLDPlusOne = dnsQuery.Name
+					}
 					testeTLDPlusOne, err := publicsuffix.EffectiveTLDPlusOne("app.sync.google.com")
 					log.Println(fmt.Sprintf("debug question:%v etld+1:%v static:%v", dnsQuery.Name,neweTLDPlusOne,testeTLDPlusOne))
 					b.NumRows++
